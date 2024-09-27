8b300bc401b8a60f0994ceeec0603a49afec40b8
m0535 <6195297+m0535@users.noreply.github.com>
2024-09-27 21:11:06 +0200

    Update AVM_Fax_Multiple_Pictures.user.js

--- AVM_Fax_Multiple_Pictures.user.js
+++ AVM_Fax_Multiple_Pictures.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-09-27
 // @description  try to take over the world!
-// @author       Stephan aus dem Hause hafiz
+// @author       Stephan aus dem Hause h.
 // @match        http*://fritz.box/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
